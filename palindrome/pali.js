5ffbc46179e2104a45bbe726e4344b0e0e0cc223
Antoniok2 <antoniopiazza18@gmail.com>
2021-10-29 16:09:47 +0200

    esercizio 1 completo senza la funzione

--- palindrome/pali.js
+++ palindrome/pali.js
@@ -2,5 +2,31 @@
 // Creare una funzione per capire se la parola inserita è palindroma.
 // Dare l’output relativo.
 
+// QUI TROVO LE VARIABILI
+var parola = prompt("Inserisci qui una parola e ti diro se è palindroma");
+var carattere;
+var parolaInversa = '';
+var i = parola.length - 1;
 
+// IN QUESTA MANIERA STO CREANDO UN CICLO WHILE PER FAR SI CHE MI SCOMPONE LA PAROLA LETTERA PER LETTA PARTENDO DALL'ULTIMA
+while (i >= 0) {
+// GLI STO DICENDO DI SCOMPORRE LA PAROLA LETTERA PER LETTERA 
+  carattere = parola[i];
+// QUI GLI STO DICENDO DI AGGIUNGERE AD OGNI GIRO OGNI SINGOLA LETTERA DELLA PAROLA SCOMPOSSA FINO A PRENDERE OGNI LETTERA AL CONTRARIO E FORMARMI LA PAROLA LETTA AL CONTRARIO
+  parolaInversa += carattere;
+  console.log(carattere);
+  i--;
+}
+
+console.log(parolaInversa);
+
+// QUINDI AGGIUNGO UNA CONDIZIONE
+// SE LA PAROLA è UGUALE ALLA PAROLA LETTA AL CONTRARIO ALLORA è PALINDROMA
+if(parola == parolaInversa){
+    alert('la parola è palindroma');
+
+    // AL CONTRARIO, NON LO è!
+  } else {
+    alert('la parola non è palindroma');
+  }
 
